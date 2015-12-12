1dcfd61d9e153983efcdc5034fa6f2d71bd236e4
Erik Tittel <et23@live.de>
2015-12-12 18:15:35 +0100

    grouping logic replaced with lambda

--- src/main/java/de/et/trips/TripService.java
+++ src/main/java/de/et/trips/TripService.java
@@ -1,8 +1,9 @@
 package de.et.trips;
 
+import static java.util.stream.Collectors.groupingBy;
+
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,36 +14,33 @@ import java.util.Map;
  */
 public class TripService {
 
+    private static final GroupFilterDriverVehicle DRIVER_VEHICLE_FILTER = new GroupFilterDriverVehicle();
+    private static final GroupFilterLocation LOCATION_FILTER = new GroupFilterLocation();
+
     /**
      * Groups a list of trips by certain properties. E. g. by driver and vehicle.
      */
     public List<TripGroup> groupTrips(List<Trip> trips) {
         List<TripGroup> groups = new ArrayList<>();
-        List<Trip> remainingTrips = addTripsToGroups(trips, groups, new GroupFilterDriverVehicle());
-        addTripsToGroups(remainingTrips, groups, new GroupFilterLocation());
+        List<Trip> remainingTrips = addTripsToGroups(trips, groups, DRIVER_VEHICLE_FILTER);
+        addTripsToGroups(remainingTrips, groups, LOCATION_FILTER);
         Collections.sort(groups);
         return groups;
     }
 
     private List<Trip> addTripsToGroups(List<Trip> trips, List<TripGroup> groups, GroupFilter groupFilter) {
         List<Trip> remainingTrips = new ArrayList<>(trips);
-        Map<GroupProperty, List<Trip>> tripsByProperty = new HashMap<>();
-        for (Trip trip : trips) {
-            GroupProperty groupProperty = groupFilter.readProperty(trip);
-            if (tripsByProperty.containsKey(groupProperty)) {
-                tripsByProperty.get(groupProperty).add(trip);
-            } else {
-                ArrayList<Trip> tripList = new ArrayList<>();
-                tripList.add(trip);
-                tripsByProperty.put(groupProperty, tripList);
-            }
-        }
-        tripsByProperty.keySet().stream().filter(p -> tripsByProperty.get(p).size() > 1).forEach(p -> {
-            List<Trip> tripsWithSameProperty = tripsByProperty.get(p);
-            groups.add(new TripGroup(p, tripsWithSameProperty));
-            remainingTrips.removeAll(tripsWithSameProperty);
-        });
 
+        Map<GroupProperty, List<Trip>> tripsByProperty = trips.stream()
+                .collect(groupingBy(groupFilter::readProperty));
+
+        tripsByProperty.keySet().stream()
+                .filter(p -> tripsByProperty.get(p).size() > 1)
+                .forEach(p -> {
+                    List<Trip> tripsWithSameProperty = tripsByProperty.get(p);
+                    groups.add(new TripGroup(p, tripsWithSameProperty));
+                    remainingTrips.removeAll(tripsWithSameProperty);
+                });
         return remainingTrips;
     }
 }
